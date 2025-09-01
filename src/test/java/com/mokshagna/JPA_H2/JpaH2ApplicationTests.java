ee03aa0b387c4ade9f7a0aa63290c6b5353dcd11
Mokshagna Reddy <c.mokshagna27@gmail.com>
2025-09-01 22:13:44 +0530

    Created structure and added dependencies

---
+++ src/test/java/com/mokshagna/JPA_H2/JpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokshagna.JPA_H2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
